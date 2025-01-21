51aeba4b6e74dd97427b3d7e97856323de7882c4
Luis Armando <armando.zumpa@gmail.com>
2025-01-20 19:22:11 -0600

    Ejercicio5 Imprime arrays

---
+++ src/ejercicio5/ImprimirArrays.java
@@ -0,0 +1,11 @@
+package ejercicio5;
+
+public class ImprimirArrays {
+
+    public void imprimirElementos(int [] numeros){
+        for (int iterador : numeros){
+            System.out.print(iterador + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/ejercicio5/MainImprime.java
@@ -0,0 +1,9 @@
+package ejercicio5;
+
+public class MainImprime {
+    public static void main(String[] args) {
+        ImprimirArrays imprimirArrays = new ImprimirArrays();
+        int [] numeros = {8, 67, 89, 78, 2, 2};
+        imprimirArrays.imprimirElementos(numeros);
+    }
+}
